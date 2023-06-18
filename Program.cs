f52b9ab0e74c5ab997e88238d8be4fe273c0ba5e
madu <maduamaral2006@gmail.com>
2023-06-18 15:49:18 -0300

    exercicio resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿int numero;
+Console.WriteLine("digite um numero");
+numero =Convert.ToInt32(Console.ReadLine());
+int resultado;
+resultado =numero % 2;
+if (resultado== 0){
+    Console.WriteLine($"{numero} é par");
+}
+else{
+    Console.WriteLine($"{numero} é impar");
+}
